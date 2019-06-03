9c942fb6b1c1404f3f4af5b946e5c85b9852903d
SoffySchmidt <sofschmi@gmail.com>
2019-06-03 17:02:32 +0200

    Added Dialogue System (WIP)

---
+++ Assets/Mixed Work/Scripts/Sofie/Dialogue.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+
+    public Animator textDisplayAnim;
+    public GameObject continueKey;
+
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    void Update()
+    {
+        if (textDisplay.text == sentences[index])
+        {
+         continueKey.SetActive(true);
+
+         if(continueKey && Input.GetKeyDown(KeyCode.E))
+            NextSentence();
+        }
+    }
+
+    public IEnumerator Type()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+    public void NextSentence()
+    {
+
+            textDisplayAnim.SetTrigger("Change");
+            continueKey.SetActive(false);
+
+            if (index < sentences.Length - 1)
+            {
+                index++;
+                textDisplay.text = "";
+                StartCoroutine(Type());
+            }
+        
+        else
+        {
+            textDisplay.text = "";
+            continueKey.SetActive(false);
+        }
+    }
+
+
+
+}
---
+++ Assets/TriggerDialogue.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDialogue : MonoBehaviour
+{
+    public GameObject Dialogue;
+    public GameObject DialogueText;
+    public Animator SpiritAnim;
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            SpiritAnim.SetBool("CircMove", true);
+            DialogueText.SetActive(true);
+            Dialogue.GetComponent<Dialogue>().enabled = true;   
+        }
+        
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            Dialogue.GetComponent<Dialogue>().enabled = false;
+            DialogueText.SetActive(false);
+
+            SpiritAnim.SetBool("CircMove", false);
+            
+        }
+    }
+}
