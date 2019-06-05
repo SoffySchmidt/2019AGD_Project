a3f7c8a9d473ec25880df910d93213206a4de0be
LauraBlicher <Lblicher@gmail.com>
2019-06-05 14:57:54 +0200

    Menu updated with new fonts

---
+++ Assets/Mixed Work/Scripts/Laura/SwitchCanvas.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+
+public class SwitchCanvas : MonoBehaviour
+{
+    public GameObject offCanvas, onCanvas, firstObject;
+
+    public void Switch()
+    {
+        offCanvas.SetActive(true);
+        onCanvas.SetActive(false);
+        GameObject.Find("EventSystem").GetComponent<EventSystem>().SetSelectedGameObject(firstObject, null);
+    }
+
+}
