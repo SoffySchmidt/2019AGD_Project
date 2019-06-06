6fe6d1b3740eac8ede50e9dbe67427f9276d0cf6
SoffySchmidt <sofschmi@gmail.com>
2019-06-06 11:47:16 +0200

    Fixed dialogue

--- Assets/Mixed Work/Scripts/Sofie/Dialogue.cs
+++ Assets/Mixed Work/Scripts/Sofie/Dialogue.cs
@@ -5,12 +5,19 @@ using TMPro;
 
 public class Dialogue : MonoBehaviour
 {
+    //Handle's text and its output
     public TextMeshProUGUI textDisplay;
+    //string array of sentences, written in the inspector of the DialogueManager
     public string[] sentences;
+    //Index number of sentence(s)
     public int index;
+    //The speed of which the text is being written out
     public float typingSpeed;
 
+    //Animator for the TextDisplay: Handles the fade-in animation which is being played
+    //every time a new sentence is shown
     public Animator textDisplayAnim;
+    //UI object for the display of Continue Key, which is merely a sprite and a text saying "Press B"
     public GameObject continueKey;
 
 
@@ -21,38 +28,53 @@ public class Dialogue : MonoBehaviour
 
     void Update()
     {
+        //If the displayed sentence of the given index number X is written out in its full length,
+        //set animation bool "Change" to false (i.e. to stay idling instead) and display the text "Press B"
+        //to show the player that it is now possible to proceed to the next step
         if (textDisplay.text == sentences[index])
         {
             textDisplayAnim.SetBool("Change", false);
             continueKey.SetActive(true);
 
-         if(continueKey && Input.GetButton("Text"))
-            NextSentence();
+            //if the continueKey text is displayed AND the input-button called "Text" has been pressed
+            //Call the method NextSentence() to play out the next sentence in the array
+            if (continueKey && Input.GetButton("Text"))
+                NextSentence();
         }
     }
-
+    //Coroutine handling the typing
     public IEnumerator Type()
     {
+        //For each letter in the sentence of index X (converted from string to char array)
+        //Add a letter to the component 'text' of the TMP object TextDisplay, so it is displayed
+        //and Wait for the time defined in "typingSpeed", before showing the next letter, etc.
+        //To give the effect of a Typewriter machine, where all the text is written in real-time.
         foreach (char letter in sentences[index].ToCharArray())
         {
-            
+
             textDisplay.text += letter;
             yield return new WaitForSeconds(typingSpeed);
         }
     }
+
+    //When the method NextSentence() is called, play the "change" animation to make the next sentence fade in. 
+    //and disable the text for the continueKey while the sentence is being written out. 
     public void NextSentence()
     {
 
-            textDisplayAnim.SetBool("Change", true);
-            continueKey.SetActive(false);
+        textDisplayAnim.SetBool("Change", true);
+        continueKey.SetActive(false);
+        //if the current index number is less than the total number of sentences in the string array (subtracted by 1, as the list starts at 0)
+        //increase the index number by 1, to proceed to the next sentence on the list. 
+        //Start the coroutine for the typing
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
 
-            if (index < sentences.Length - 1)
-            {
-                index++;
-                textDisplay.text = "";
-                StartCoroutine(Type());
-            }
-        
+        //Otherwise, let the last sentence in the index stay displayed and set the continueKey to false, to indicate that the dialogue is finished
         else
         {
             textDisplay.text = "";
--- Assets/Mixed Work/Scripts/Sofie/TriggerDialogue.cs
+++ Assets/Mixed Work/Scripts/Sofie/TriggerDialogue.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class TriggerDialogue : MonoBehaviour
 {
+    //The DialogueManager contains all components and information regarding the dialogue
     public GameObject DialogueManager;
+    //TextMeshPro object handling the written text's appearance.
     public GameObject DialogueText;
+    //UI showing 'Press B' 
     public GameObject ContinueKey;
+    //Spirit's animator
     public Animator SpiritAnim;
-    int indexNum;
+
+    //Timer variables used for the delay needed after the first encounter with the spirit.
+    //During this short delay, everything is disabled, in order to avoid the continuation
+    //of text display until the Player has reached the second checkpoint.
+    float timer = 0;
+    float timeToWait = 1f;
+
     // Update is called once per frame
 
     private void Update()
     {
+        //Index of sentences on the DialogueManager
+        //For each sentence, a sprite animation is played.
         if (DialogueManager.GetComponent<Dialogue>().index == 0)
         {
             SpiritAnim.Play("WaterSpirit_Idle");
@@ -32,45 +44,85 @@ public class TriggerDialogue : MonoBehaviour
         {
             SpiritAnim.Play("WaterSpirit_FlyIdle");
         }
+        //Disable() is called to ensure that the last sentence of the spirit's index is not being played
+        //before it reaches the second checkpoint 
         if (DialogueManager.GetComponent<Dialogue>().index == 4)
         {
+            Disable();
             SpiritAnim.Play("WaterSpirit_FlyOff");
         }
+
+        //After the last sentence has been written out, all components related to the spirit's dialogue system
+        //are being disabled/set to false. What is left is merely a non-interactable floating spirit.
         if (DialogueManager.GetComponent<Dialogue>().index == 6)
         {
             SpiritAnim.Play("WaterSpirit_FlyOff2");
-            //DialogueText.SetActive(false);
             ContinueKey.SetActive(false);
             DialogueManager.GetComponent<Dialogue>().enabled = false;
             gameObject.GetComponent<CircleCollider2D>().radius = 1;
             gameObject.GetComponent<CircleCollider2D>().isTrigger = false;
         }
-    }
 
+    }
+    //Checking if the object tagged "Player" has entered or exited the spirit's CircleCollider
+    //Which enables or disables the attached components and variables. 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "Player")
         {
-            gameObject.GetComponent<CircleCollider2D>().radius = 15;
-            DialogueManager.GetComponent<Dialogue>().enabled = true;
             DialogueText.SetActive(true);
             ContinueKey.SetActive(true);
-            
+            DialogueManager.GetComponent<Dialogue>().enabled = true;
+            gameObject.GetComponent<CircleCollider2D>().radius = 15;
 
         }
-        
+
     }
-    
+
     void OnTriggerExit2D(Collider2D col)
     {
         if (col.tag == "Player")
         {
-            gameObject.GetComponent<CircleCollider2D>().radius = 9;
+            gameObject.GetComponent<CircleCollider2D>().radius = 5;
             DialogueManager.GetComponent<Dialogue>().enabled = false;
             DialogueText.SetActive(false);
             ContinueKey.SetActive(false);
         }
     }
-    
+
+    //Called when the last sentence of the first encounter with the spirit is finished.
+    void Disable()
+    {
+        //Spirit's collider
+        Collider2D col = gameObject.GetComponent<CircleCollider2D>();
+
+        //Running timer 
+        timer += Time.deltaTime;
+
+        //If the timer has not reached the pre-defined time count, disable all objects 
+        //to avoid the last sentence to be played. This is being done during the time
+        //that the spirit's flies off to the 2nd checkpoint.
+        if (timer <= timeToWait)
+        {
+            ContinueKey.SetActive(false);
+            DialogueText.SetActive(false);
+            DialogueManager.GetComponent<Dialogue>().enabled = false;
+        }
+        //If the timer has reached the pre-defined time count AND if Player has trigger-entered the spirit's collider
+        //enable everything to allow the player to activate the last sentence of the index.
+        if (timer >= timeToWait)
+        {
+
+            if (col.tag == "Player")
+            {
+                DialogueText.SetActive(true);
+                ContinueKey.SetActive(true);
+                DialogueManager.GetComponent<Dialogue>().enabled = true;
+                gameObject.GetComponent<CircleCollider2D>().radius = 15;
+            }
+
+        }
+
+    }
 
 }
